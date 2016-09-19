712efc2fe14698706758f5cd76fac5f8b61a6609
caveman07 <ram07kbr90@gmail.com>
2016-09-19 20:15:43 +0300

    added posts styling and other user profile features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,21 @@ module ApplicationHelper
   def current_user?(user)
     return true if current_user == user
   end
+
+  def count_all_likes_of_posts_by (user)
+      likes = 0
+      user.posts.each do |post|
+        likes += post.likes.count
+      end
+      likes
+  end
+
+  def count_all_comments_of_posts_by (user)
+      comments = 0
+      user.posts.each do |post|
+        comments += post.comments.count
+      end
+      comments
+    end
+
 end
