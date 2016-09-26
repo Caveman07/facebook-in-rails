75746cb194c42ef0da23b5997b5f0f2e0cd55182
caveman07 <ram07kbr90@gmail.com>
2016-09-26 10:44:55 +0300

    added screenshots

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"],
+    :aws_access_key_id      => 'AKIAJYVYE2DCLFJKXV7A',                        # required
+    :aws_secret_access_key  => '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',
     :region                => 'us-west-2'                        # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]                     # required
+  config.fog_directory  = 'uzls1'                    # required
 end
